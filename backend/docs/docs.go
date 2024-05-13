854ae87bcf2d32ee60cec90ef68f46db33393100
Haidar Jbeily <h.jbeily@innopolis.university>
2024-05-13 21:36:33 +0300

    release v1.2

--- backend/docs/docs.go
+++ backend/docs/docs.go
@@ -542,7 +542,7 @@ type swaggerInfo struct {
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = swaggerInfo{
-	Version:     "1.0",
+	Version:     "1.2",
 	Host:        "",
 	BasePath:    "/api",
 	Schemes:     []string{},
